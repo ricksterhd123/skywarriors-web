2d723e6b4eca8923a93019604e0f0bdc664e5824
ricksterhd123 <ricksterhd123@gmail.com>
2019-02-09 03:21:53 +0000

    bug fixing

--- public/javascripts/list.js
+++ public/javascripts/list.js
@@ -2,25 +2,22 @@
  *  Create a list of players from player info object
  */
 function createPlayerList(playerInfo){
-    var listTitle = document.getElementById('listTitle');
-    var listItems = document.getElementsByClassName('listItem');
-    if (listTitle){ document.body.removeChild(listTitle); }
-    if (listItems.length > 0){
-        for (let i = 0; i <= listItems.length - 1; i++) {
-            document.body.removeChild(listItems[0]);
-        }
-    }
+    var div = document.getElementById('playerList');
+    if (div) { document.body.removeChild(div); }
 
+    var div = document.createElement('div');
+    div.id = "playerList";
 
     var listTitle = document.createElement('h2');
-    listTitle.id = "listTitle";
     listTitle.innerHTML = `Players online (${playerInfo.length})`;
-    document.body.appendChild(listTitle);
+
+    div.appendChild(listTitle);
 
     for (let i = 0; i <= playerInfo.length - 1; i++){
         let listItem = document.createElement('li');
-        listItem.className = "listItem";
         listItem.innerHTML = playerInfo[i].name;
-        document.body.appendChild(listItem);
+        div.appendChild(listItem);
     }
+
+    document.body.appendChild(div);
 }
