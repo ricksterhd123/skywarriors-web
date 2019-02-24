8ea9239fc92165cb87fcb00a5ec7323bb3584f14
ricksterhd123 <ricksterhd123@gmail.com>
2019-02-24 20:06:53 +0000

    Update players.js

--- routes/players.js
+++ routes/players.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const Client = require('mtasa').Client;
-const mta = new Client("35.247.225.180", 22005, "webapi", "test1234");
+const mta = new Client("35.246.82.199", 22005, "webapi", "test1234")
 
 /**
  * Call the exported function 'getPlayersOnline' from the MTA server and
